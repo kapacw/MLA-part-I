ad0fa264adc6d6ec9ec3330f454dd172e5aa6172
Kayque <kkayque.w2@gmail.com>
2024-07-29 16:43:25 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function pontLogo(on1){
+    on1.style.cursor = "pointer";
+}
+function ponteiro(on2){
+    on2.style.cursor = "pointer";
+    on2.style.padding = "7px";
+}
+function ponteiroOff(off){
+    off.style.padding = "0px";
+}
